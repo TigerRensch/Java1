8c7be82145f952642be4aa993d93cee90c74e901
Sachin <shaldipur79@gmail.com>
2017-06-11 09:11:40 -0400

    Lesson 9 Exc 2 files

--- Lesson 9/StudentSearcher2.java
+++ Lesson 9/StudentSearcher2.java
@@ -14,7 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class StudentSearcher{
+public class StudentSearcher2{
 
       //fields
       String firstName;
