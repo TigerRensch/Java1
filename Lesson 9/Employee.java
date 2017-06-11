6fedd23ad7053ae8b7944f7b0a6160cbb05dbed1
Sachin <shaldipur79@gmail.com>
2017-06-11 09:45:11 -0400

    Deleted some files

---
+++ Lesson 9/Employee.java
@@ -0,0 +1,50 @@
+//Author: Sachin Haldipur
+//Date: 06/11/201
+//Class Info: CIS163AA - Java Programming: Level I
+//Lesson 9
+//Exercise 2
+/*Description of Program:
+ *
+ *This class will be used with the EmployeeData class
+ *
+ */
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int numericID;
+    private double hourlyWage;
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public int getNumericID() {
+        return numericID;
+    }
+
+    public void setNumericID(int numericID){
+        this.numericID = numericID;
+    }
+
+    public double getHourlyWage(){
+        return hourlyWage;
+    }
+
+    public void setHourlyWage(double hourlyWage){
+        this.hourlyWage = hourlyWage;
+    }
+}
