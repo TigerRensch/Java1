825beac8a1ec8e9354e3f16ea3e7f3008a8a23d3
Sachin <shaldipur79@gmail.com>
2017-06-18 09:02:23 -0400

    More code for exc 2

--- Lesson 9/EmployeeData.java
+++ Lesson 9/EmployeeData.java
@@ -10,34 +10,34 @@
  */
 
 
-import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+
 
 
 public class EmployeeData {
 
     public static void main(String[]args){
 
-        //boolean search = true;
-        int numEmployees;
+        boolean search = true;
+        int numEmployees = 0;
         int employeeNum = 0;
         String employeeData;
-        ArrayList<String> itemList = new ArrayList<String>();
-        ArrayList<String> employeeList = new ArrayList<String>();
-        String path = "employees.txt";
         String firstName = "";
         String lastName = "";
         String numericID = "";
         String hourlyWage = "";
+        String[] employeeItemArray = new String[4];
+        ArrayList<String> employeeList = new ArrayList<>();
 
 
-        Scanner input = new Scanner(System.in);
 
-        Employee[] emps = new Employee[7];
+        while(search){
+
+            Scanner input = new Scanner(System.in);
+
 
             System.out.print("\nHow many employees do you want to enter: >>");
             numEmployees = input.nextInt();
@@ -47,41 +47,46 @@ public class EmployeeData {
             System.out.print("\nPlease enter the Employee's Data separated by exactly one space first name, last name, numeric ID and hourly wage >>");
             employeeData = input.nextLine();
 
-            Employee employee = new Employee();
-            String[] itemSeparator = employeeData.split(" ");
-
-            for(int i=0;i<itemSeparator.length;i++){
-                String item = itemSeparator[i];
-
-                if(i == 0){
-                    employee.setFirstName(item);
-                    firstName = employee.getFirstName();
-                }
-
-                if(i == 1){
-                    employee.setLastName(item);
-                    lastName = employee.getLastName();
-                }
-
-                if(i == 2){
-                    employee.setNumericID(item);
-                    numericID = employee.getNumericID();
-                }
-
-                if(i == 3){
-                    employee.setHourlyWage(item);
-                    hourlyWage = employee.getHourlyWage();
-                }
-
-            }
+            employeeItemArray = employeeData.split(" ");
+            employeeList.addAll(Arrays.asList(employeeItemArray));
 
             employeeNum ++;
 
-            System.out.print("Employee # " + employeeNum);
-            System.out.print("\nEmployee Name: " + firstName + "  " + lastName);
-            System.out.print("\nID: " + numericID);
-            System.out.print("\nHourly wage: " + hourlyWage);
+            if(employeeNum > 0){
 
+                if(employeeNum != numEmployees){
+                    System.out.print("\nInput the next Employee >>");
+                    employeeData = input.nextLine();
+
+                    employeeItemArray = employeeData.split(" ");
+                    employeeList.addAll(Arrays.asList(employeeItemArray));
+                }
+                else if(employeeNum == numEmployees){
+                    for(int i=0; i < employeeList.size(); i++){
+
+                        Employee employee = new Employee();
+
+                        for(int j = 0; j < employeeItemArray.length; j++){
+                            String item = employeeItemArray[j];
+
+                            if(j == 0){
+                                employee.setFirstName(item);
+                                firstName = employee.getFirstName();
+                            }
+
+                        }
+
+                        System.out.print("\nEmployee # " + employeeNum);
+
+                        System.out.print("\nEmployee Name: " + firstName);
+
+                    }
+                }
+            }
+
+        }
 
     }
+
+
 }
