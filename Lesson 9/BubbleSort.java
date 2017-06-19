2aeeb233e72f8bd4048a7753ba57004cf6eb0643
Sachin <shaldipur79@gmail.com>
2017-06-19 07:21:12 -0400

    Added sort methods into classes for Lesson 9 exercise 3

--- Lesson 9/BubbleSort.java
+++ Lesson 9/BubbleSort.java
@@ -1,5 +1,32 @@
-/**
- * Created by sachinhaldipur on 6/18/17.
- */
+//Author: Sachin Haldipur
+//Date: 06/11/2017
+//Class Info: CIS163AA - Java Programming: Level I
+//Lesson 9
+//Exercise 2
+/*Description of Program:
+*
+*
+*
+*
+*/
 public class BubbleSort {
+    public void bubbleSort(){
+
+        int []someNums = new int [5];
+        int temp = 0;
+
+        for(int a=0; a < someNums.length-1; ++a){
+            for(int b=0; b<someNums.length-1; ++a){
+                if(someNums[b] > someNums[b+1]){
+                    temp = someNums[b];
+                    someNums[b] = someNums[b+1];
+                    someNums[b+1] = temp;
+                }
+
+            }
+        }
+
+
+    }
+
 }
--- Lesson 9/InsertionSort.java
+++ Lesson 9/InsertionSort.java
@@ -2,4 +2,25 @@
  * Created by sachinhaldipur on 6/18/17.
  */
 public class InsertionSort {
+
+    public void insertionSort(){
+
+        int []someNums = new int [5];
+        int temp = 0;
+
+        int a=1;
+
+        while(a < someNums.length){
+            temp = someNums[a];
+            int b = a-1;
+            while(b >=0 && someNums[b] > temp){
+                someNums[b+1]=someNums[b];
+                --b;
+            }
+            someNums[b+1]=temp;
+            ++a;
+
+        }
+
+    }
 }
