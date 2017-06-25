cc2d78eeb1243e7d09b178befdf5ab968021e6a8
Sachin <shaldipur79@gmail.com>
2017-06-24 20:15:23 -0400

    Chapter 11 exercise 2 changes

--- Lesson 11/Contacts.java
+++ Lesson 11/Contacts.java
@@ -1,5 +1,65 @@
 /**
  * Created by sachinhaldipur on 6/24/17.
  */
-public class Contacts {
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.table.DefaultTableModel;
+
+
+public class Contacts extends JFrame implements ActionListener {
+
+    JTextField name = new JTextField(12);
+    JTextField phoneNumber = new JTextField(12);
+    JButton add = new JButton("Add");
+    JButton save = new JButton("Save");
+
+    JTable table = new JTable();
+
+
+    DefaultTableModel model = new DefaultTableModel();
+
+    public Contacts(){
+
+        super("Add Contacts");
+        setSize(1000,1000);
+        setLayout(new FlowLayout());
+
+        add(name);
+        add(phoneNumber);
+        add(add);
+        add(save);
+
+
+        table.setModel(model);
+        model.addColumn("Name");
+        model.addColumn("Phone Number");
+
+        add(table);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add.addActionListener(this);
+        save.addActionListener(this);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        String n = name.getText();
+        String pn = phoneNumber.getText();
+
+
+
+        if(e.getActionCommand().equals("Add")){
+            model.addRow(new Object[]{n, pn});
+        }
+
+        if(e.getActionCommand().equals("Save")){
+
+        }
+    }
+
 }
---
+++ Lesson 11/ContactsDemo.java
@@ -0,0 +1,12 @@
+/**
+ * Created by sachinhaldipur on 6/24/17.
+ */
+public class ContactsDemo {
+
+    public static void main(String[]args){
+
+        Contacts contacts = new Contacts();
+        contacts.setVisible(true);
+
+    }
+}
